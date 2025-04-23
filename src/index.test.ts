2c0f0d23e0dcc1be1d6488d45d94902a348d50f5
xinchentechnote <283235702@qq.com>
2025-04-23 15:10:57 +0800

    fix:unittest

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,7 +3,3 @@ import { add } from './index';
 test('add returns correct greeting', () => {
     expect(add(1, 2)).toBe(3);
 });
-
-test('add returns correct greeting', () => {
-    expect(add(12, 3)).toBe(3);
-});
